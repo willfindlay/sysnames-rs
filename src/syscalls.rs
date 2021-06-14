e0cf446282b2b096526c51285c2667698a1b1585
William Findlay <william@williamfindlay.com>
2021-06-14 15:04:31 -0400

    Export syscalls to $OUT_DIR

---
+++ src/syscalls.rs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+//
+// Linux system call names and numbers.
+// Copyright (c) 2021  William Findlay
+//
+// June 14, 2021  William Findlay  Created this.
+
+#![allow(missing_docs)]
+
+use std::env;
+use std::include;
+
+include!(concat!(env!("OUT_DIR"), "/syscalls.rs"));
